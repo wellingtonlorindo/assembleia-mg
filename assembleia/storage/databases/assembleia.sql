a39716200f73c00874ba468f127ac6db0441c571
wellington lorindo <wellingtonlorindo@gmail.com>
2015-08-24 11:07:29 -0300

    Removendo tabelas deputados e verbas_indenizatorias

--- assembleia/storage/databases/assembleia.sql
+++ assembleia/storage/databases/assembleia.sql
@@ -1,17 +1,5 @@
 BEGIN TRANSACTION;
 
-CREATE TABLE deputados (id INTEGER PRIMARY KEY, nome varchar(50), partido varchar(10));
-
 CREATE TABLE tipos_despesas (codTipoDespesa INTEGER PRIMARY KEY, descTipoDespesa VARCHAR(100));
-
-CREATE TABLE verbas_indenizatorias (
-	idVerba INTEGER PRIMARY KEY,
-	idDeputado INTEGER,
-	dataReferencia TEXT,
-	codTipoDespesa INTEGER,
-	valor NUMERIC,
-	FOREIGN KEY(idDeputado) REFERENCES deputados(id),
-	FOREIGN KEY(codTipoDespesa) REFERENCES tipos_despesas(codTipoDespesa)
-);
 
 COMMIT;
